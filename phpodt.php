95023528e25006243bb73a4ef209306552073d16
cakyus <cakyus@gmail.com>
2020-09-24 23:18:12 +0700

    use absolute file path

--- phpodt.php
+++ phpodt.php
@@ -3,16 +3,17 @@
 include 'functions.php';
 
 function odt_autoload($classname) {
-  $classname = strtolower($classname);
-  $path = "class.$classname.php";
-  if (strpos($classname, 'exception')) {
-    $path = "exceptions/class.$classname.php";
-  }
-  include_once $path;
+	$classname = strtolower($classname);
+	$path = "class.$classname.php";
+	if (strpos($classname, 'exception')) {
+		$path = "exceptions/class.$classname.php";
+	}
+	if (is_file(dirname(__FILE__).'/'.$path)){
+		include_once(dirname(__FILE__).'/'.$path);
+	}
 }
 
 spl_autoload_extensions('.php');
 spl_autoload_register('odt_autoload');
 
 
-?>
